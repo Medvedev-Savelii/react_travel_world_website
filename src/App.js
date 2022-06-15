10a1f903d8d1f1bd15b8a14cd6ed345b2fd82fba
sava <savamedvedev@gmail.com>
2022-06-16 00:30:19 +0800

    4-commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Destinations from "./components/Destinations";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Services from "./components/Services";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Services />
+      <Destinations />
     </>
   );
 }
