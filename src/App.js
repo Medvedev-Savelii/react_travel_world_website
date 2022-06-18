eef0b990c6fd90da1dfe24ca36c59477c9b64f0b
sava <savamedvedev@gmail.com>
2022-06-18 12:38:56 +0800

    6-commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import Destinations from "./components/Destinations";
+import DownloadApp from "./components/DownloadApp";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Offer from "./components/Offer";
 import Services from "./components/Services";
+import Testimonial from "./components/Testimonial";
+import Tours from "./components/Tours";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Home />
       <Services />
       <Destinations />
+      <Offer />
+      <Tours />
+      <Testimonial />
+      <DownloadApp />
     </>
   );
 }
